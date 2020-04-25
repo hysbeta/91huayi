ec367785b2f12ede4e3ba450aaa62e3aac59d8b6
hysbeta <30502871+hysbeta@users.noreply.github.com>
2020-04-25 23:08:46 +0800

    Create cme_91huayi_class.js

---
+++ cme_91huayi_class.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         cme_91huayi_class
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*.91huayi.com/course_ware/course_ware_cc.aspx*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict'
+    console.log('Script Ready!');
+    var fuc = setInterval(function() {
+        if (document.querySelector("#jrks").getAttribute("disabled")==null){
+            document.querySelector("#jrks").click();
+            console.log('OK');
+        }else{
+            console.log('Still playing video...');
+        }
+    },1000)
+})();
