b1553d3c8734faeaf5c6119987587dc484e5def0
Fonghang <30502871+hysbeta@users.noreply.github.com>
2020-04-25 23:58:41 +0800

    Update cme_91huayi_next_class.js

--- cme_91huayi_next_class.js
+++ cme_91huayi_next_class.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       VX:Acdtms4zfx
 // @match        http://*.91huayi.com/pages/exam_result.aspx*
 // @grant        none
 // ==/UserScript==
