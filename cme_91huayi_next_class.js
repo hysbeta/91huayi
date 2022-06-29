b43d7a5c47282ee9e5402f10bab1b153fd1cff44
Fonghang <30502871+hysbeta@users.noreply.github.com>
2022-06-29 22:30:17 +0800

    Update cme_91huayi_next_class.js

--- cme_91huayi_next_class.js
+++ cme_91huayi_next_class.js
@@ -11,7 +11,12 @@
 (function() {
     'use strict'
     alert = console.log;
-    setTimeout(function(){console.log('cme_91huayi_next_class');},5000);
+    function sleep(time, unit){
+        if(time == null){time = 5000;}//我想不带参数的时候就默认5秒
+        if(unit != null){time = time * 1000;}//我想这个参数是任意字符时，前面的就是秒，当然，真要在别处用，这里要再改改
+        for(var t = Date.now();Date.now() - t <= time;);
+    }
+    sleep();
     if (document.querySelector("body > div.case4 > b").textContent=="考试通过！"){
         var nodenumber = document.querySelector("body > div.case3 > div.left > dl").childElementCount;
         var i = 2
