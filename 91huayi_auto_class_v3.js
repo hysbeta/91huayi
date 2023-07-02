98ecb4d826e39e52db35d8b018c123999ebfc773
Fonghang <30502871+hysbeta@users.noreply.github.com>
2023-07-02 12:06:17 +0800

    Update 91huayi_auto_class_v3.js

--- 91huayi_auto_class_v3.js
+++ 91huayi_auto_class_v3.js
@@ -39,20 +39,25 @@
                 enableStartExam();
             };
         };
-        document.querySelector("#video > div > div.pv-video-wrap > video").muted=true;
-        document.querySelector("#video > div > div.pv-video-wrap > video").play();
-        closeBangZhu();
+        document.querySelector("#video > div > div.pv-video-wrap > video").muted=true; //启用未交互后台播放
+        document.querySelector("#video > div > div.pv-video-wrap > video").play(); //播放
+        closeBangZhu(); //关闭提示弹窗
+        if (document.querySelector("#video > div > div.pv-line-tips") != null){
+            document.querySelector("#video > div > div.pv-line-tips > a").click();
+        }; //切换线路
         if (document.querySelector("#video > div > div.pv-ask-modal-wrap") != null){
             document.querySelector("#video > div > div.pv-ask-modal-wrap").remove();
-        };
+        }; //清除问题
         if (document.querySelector("#video > div > div.pv-skin-blue.pv-video-bottom.pv-subtitle-hide.pv-base-control.pv-stream-hide > div.pv-controls > div.pv-controls-right > div:nth-child(3) > button").className.indexOf('pv-icon-volumeon') != -1){
             document.querySelector("#video > div > div.pv-skin-blue.pv-video-bottom.pv-subtitle-hide.pv-base-control.pv-stream-hide > div.pv-controls > div.pv-controls-right > div:nth-child(3) > button").click();
-        };
-        initialSign();
-        document.querySelector("#video > div > div.sign-in-menu > div > div.sign-in-wrap > div.sign-in-wrap_btn > span").click();
+        }; //静音
+        if (document.querySelector("#video > div > div.sign-in-menu") == null){
+            initialSign();
+        }; //调用签到保活（保持播放）
+        document.querySelector("#video > div > div.sign-in-menu > div > div.sign-in-wrap > div.sign-in-wrap_btn > span").click(); //签到
         if (document.querySelector("#video > div > div.pv-skin-blue.pv-video-bottom.pv-subtitle-hide.pv-base-control.pv-stream-hide > div.pv-controls > div.pv-controls-left > button").className.indexOf('pv-icon-btn-play') != -1){
             document.querySelector("#video > div > div.pv-skin-blue.pv-video-bottom.pv-subtitle-hide.pv-base-control.pv-stream-hide > div.pv-controls > div.pv-controls-left > button").click();
-        };
+        }; //暂停续播
         document.querySelector("#jrks").click();
     },wait_time * 1000);
 })();
