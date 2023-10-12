99df76222bcc65e9b21dc638361843fa646b71dd
Fonghang <30502871+hysbeta@users.noreply.github.com>
2023-10-13 00:47:25 +0800

    Update and rename 91huayi_auto_exam_chk_ans_v2.js to 91huayi_auto_exam_chk_ans.js

--- 91huayi_auto_exam_chk_ans.js
+++ 91huayi_auto_exam_chk_ans.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         91huayi_auto_exam_chk_ans
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      1.0
 // @description  91huayi_auto_exam_chk_ans_
 // @author       Acdtms4zfx
 // @match        *://*.91huayi.com/pages/exam_result.aspx?*
